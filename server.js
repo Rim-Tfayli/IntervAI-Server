dfdc99a9d3bf0d7b818f7ab72db410a975921a67
Rim Tfayli <rim.tf.2004@hotmail.com>
2026-08-02 13:32:06 +0300

    added db connection file - called it in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
+connectDB()
     .then(() => {
         console.log("MongoDB connected");
         app.listen(PORT, () => {
